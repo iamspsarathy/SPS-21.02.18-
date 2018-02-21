9f901e93f0659aa815a1a5dd828dbbf752d0a1e2
iamspsarathy <35688629+iamspsarathy@users.noreply.github.com>
2018-02-21 16:49:23 +0530

    Create 21.02.18.2.C

---
+++ 21.02.18.2.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+printf("Enter the given number a");
+scanf("%d"&n,a);
+printf("enter the given number b");
+scanf("%d"&n,b)';
+getch()
+}
+return 0;
+}
+ }
