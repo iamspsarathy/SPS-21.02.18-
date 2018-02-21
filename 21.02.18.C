174d3b72daa1dbb503157b4a5438bbe607db8521
iamspsarathy <35688629+iamspsarathy@users.noreply.github.com>
2018-02-21 16:08:54 +0530

    Create 21.02.18.C

---
+++ 21.02.18.C
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <conio.h>
+int main()
+{
+print("Enter the no of hours");
+scanf("%d"&n,a);
+printf("Enter the no of minutes");
+scanf("%d&n,b");
+
+getch();
+}
+return 0;
+}
+}
